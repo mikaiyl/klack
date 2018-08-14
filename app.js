64915ea4e3428af097bb410830b58e69074ca88c
Mikaiyl <mikaiyl.davis@icloud.com>
2018-08-14 04:41:47 -0400

    almost

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+const PORT = 3000
 const express = require('express')
-const querystring = require('querystring');
-const port = 3000
+const querystring = require('querystring')
 const app = express()
+const mongoose = require('mongoose')
+mongoose.connect( 'mongodb://localhost:27017/klack', { useNewUrlParser: true })
+
+const Message = mongoose.model( 'Message',{
+    message: String,
+    sender: String,
+    timestamp: Date,
+})
 
 // List of all messages
 let messages = []
@@ -14,35 +22,36 @@ app.use(express.json())
 
 // generic comparison function for case-insensitive alphabetic sorting on the name field
 function userSortFn(a, b) {
-    var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-    var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    var nameA = a.name.toUpperCase() // ignore upper and lowercase
+    var nameB = b.name.toUpperCase() // ignore upper and lowercase
     if (nameA < nameB) {
-      return -1;
+        return -1
     }
     if (nameA > nameB) {
-      return 1;
+        return 1
     }
-  
+
     // names must be equal
-    return 0;      
+    return 0
 }
 
 app.get("/messages", (request, response) => {
     // get the current time
-    const now = Date.now();
+    const now = Date.now()
 
     // consider users active if they have connected (GET or POST) in last 15 seconds
     const requireActiveSince = now - (15*1000)
 
+    // Message.findOne({},{},{sort: {'timestamp':-1}}).then( message => message.timestamp ).then( timestamp => {
     // create a new list of users with a flag indicating whether they have been active recently
     usersSimple = Object.keys(users).map((x) => ({name: x, active: (users[x] > requireActiveSince)}))
 
     // sort the list of users alphabetically by name
-    usersSimple.sort(userSortFn);
+    usersSimple.sort(userSortFn)
     usersSimple.filter((a) => (a.name !== request.query.for))
 
     // update the requesting user's last access time
-    users[request.query.for] = now;
+    users[request.query.for] = now
 
     // send the latest 40 messages and the full user list, annotated with active flags
     response.send({messages: messages.slice(-40), users: usersSimple})
@@ -54,8 +63,9 @@ app.post("/messages", (request, response) => {
     request.body.timestamp = timestamp
 
     // append the new message to the message list
-    messages.push(request.body)
-
+    //messages.push(request.body)
+    const message = new Message( request.body )
+    message.save()
     // update the posting user's last access timestamp (so we know they are active)
     users[request.body.sender] = timestamp
 
@@ -64,4 +74,4 @@ app.post("/messages", (request, response) => {
     response.send(request.body)
 })
 
-app.listen(3000)
+app.listen( PORT )
